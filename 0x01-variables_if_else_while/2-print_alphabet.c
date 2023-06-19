69a843c9607ac024d90af13b12b56a65fa45642f
Francis <francisonah2018@gmail.com>
2023-06-18 23:38:26 -0700

    Lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that print lower case alphabets using putchar function
+ * Return: 0 (Successs)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
