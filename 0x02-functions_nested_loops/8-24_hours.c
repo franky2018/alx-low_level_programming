23230c15898efcb732fe2916c476fa359312a323
Francis <francisonah2018@gmail.com>
2023-06-21 10:54:05 -0700

    Hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
  *
  * Return: no return
  */
-int jack_bauer(void)
+void jack_bauer(void)
 {
 	int a, b, c, d;
 
@@ -17,7 +17,7 @@ int jack_bauer(void)
 				for (d = 0; d <= 9; d++)
 				{
 					if (a >= 2 && b >= 4)
-					break;
+						break;
 					_putchar(a + 48);
 					_putchar(b + 48);
 					_putchar(58);
