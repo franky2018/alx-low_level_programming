b875f47bf8bef2255f22cc6d05da28cd0156d571
Francis <francisonah2018@gmail.com>
2023-06-22 03:01:20 -0700

    Add two value

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,2 +1,12 @@
 #include "main.h"
+/**
+ * add - Add two values
+ * @n1: The first number to be completed
+ * @n2: The second number to be completed
+ * Return: integer value
+ */
 
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
