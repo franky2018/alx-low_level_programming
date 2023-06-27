5a8d1c40d35038c176c6e1137b90c7d0dddcf843
Francis <francisonah2018@gmail.com>
2023-06-27 02:10:12 -0700

    Put2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - Prints every other character of the string
+ * @str: The string to be treated
+ * Return: void
+ */
+void put2(char *str)
+{
+	int i;
+	int j;
+
+	while (str[j] '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
