9fa10965e965756005f5e64921e4d9caf8c022fd
Francis <francisonah2018@gmail.com>
2023-07-06 14:54:56 -0700

    Hot fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -33,3 +33,17 @@ int comparator(char *s, int n1, int n2)
 	}
 	return (0);
 }
+
+/**
+ * is_palindrome - Detects if a string is a palindrome
+ * @s: string
+ *
+ * Return:1 if s is palindrome, 0 if not.
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
+}
